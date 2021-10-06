60393dfac78f026695b63a5c1476c7fcdab11cc8
Ralf D. Müller <ralf.d.mueller@gmail.com>
2021-10-06 11:53:55 +0200

    updated to v2.0.1

--- dtcw.bat
+++ dtcw.bat
@@ -9,8 +9,8 @@ exit /b
 
 $main_config_file = "docToolchainConfig.groovy"
 # $version=ng
-$version = "2.0.0"
-$dockerVersion = "2.0.0"
+$version = "2.0.1"
+$dockerVersion = "2.0.1"
 $distribution_url = "https://github.com/docToolchain/docToolchain/releases/download/v$version/docToolchain-$version.zip"
 
 $dtc_opts="$dtc_opts -PmainConfigFile='$main_config_file' --warning-mode=none"
