0157847fab8095873f58c70f988b206c581a974d
Ralf D. Müller <ralf.d.mueller@deutschebahn.com>
2022-07-05 12:49:53 +0200

    Update dtcw.ps1

--- dtcw.ps1
+++ dtcw.ps1
@@ -14,8 +14,49 @@ $dtc_opts="$env:dtc_opts -PmainConfigFile='$main_config_file' --warning-mode=non
 $home_path = $env:USERPROFILE
 $folder_name = ".doctoolchain"
 $dtcw_path = "$home_path\$folder_name"
- 
-Write-Host "dtcw - docToolchain wrapper V0.23 (PS)"
+$doJavaCheck = $True
+
+function checkJava()
+{
+    if (Get-Command java -ErrorAction SilentlyContinue)
+    {
+        $java = $True
+        $javaversion = (Get-Command java | Select-Object -ExpandProperty Version).toString()
+        echo "Java Version $javaversion"
+    }
+    else
+    {
+        # Text adapted
+        Write-Warning @'
+docToolchain depends on java, but the java command couldn't be found.
+
+To install java, you can type
+
+./dtcw getJava
+
+to invoke an automatic installer. This will install JDK 11 only for docToolchain without interfering with your current Java installations.
+
+...or you could follow the next link and manually install JDK 11 on your system for global use:
+
+>> https://adoptium.net/
+please choose Temurin 11
+'@
+        exit 1
+    }
+}
+
+if (Test-Path "$dtcw_path\jdk-11.0.15+10" ) {
+    Write-Host "local java JDK-11 found"
+    $java = $True
+    $dtc_opts="$dtc_opts '-Dorg.gradle.java.home=$dtcw_path\jdk-11.0.15+10' "
+    if (test-path env:JAVA_HOME) {
+    } else {
+        $env:JAVA_HOME="$dtcw_path\jdk-11.0.15+10"
+    }
+    $doJavaCheck = $False
+}
+
+Write-Host "dtcw - docToolchain wrapper V0.24 (PS)"
 
 if ($args.Count -lt 1) {
     # Help text adapted to Win/PS: /<command>.ps1
@@ -48,21 +89,6 @@ Examples:
 # check if CLI or docker are installed:
 $cli = $docker = $exist_home =  $False
 
-if (Get-Command java -ErrorAction SilentlyContinue) {    
-    $java = $True
-    $javaversion = (Get-Command java | Select-Object -ExpandProperty Version).toString()
-    echo "Java Version $javaversion"
-} else {    
-    # Text adapted
-    Write-Warning @'
-docToolchain depends on java, but the java command couldn't be found to install java. 
-Please, follow the next link and install java:
->> https://adoptium.net/
-please choose Temurin 11
-'@
-    exit 1
-}
-
 if (Get-Command dooctoolchain -ErrorAction SilentlyContinue) {        
     Write-Host "docToolchain as CLI available"
     $cli = $True
@@ -88,6 +114,18 @@ switch ($args[0]) {
         Write-Host "force use of docker"
         $cli = $exist_home = $False
         $firstArgsIndex = 1   # << Shift first param
+    }
+    "getJava" {
+        Write-Host "this script assumes that you have a 64 bit Windows installation"
+        Write-Host "it now tries to install Java for you"
+        New-Item -Path $home_path -Name $folder_name -ItemType "directory" -Force | Out-Null
+        $jdkDistribution = "https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.15%2B10/OpenJDK11U-jdk_x64_windows_hotspot_11.0.15_10.zip"
+        Write-Host "downloadting JDK temurin11 from adoptiom to $dtcw_path/jdk.zip"
+        Invoke-WebRequest $jdkDistribution -OutFile "$dtcw_path\jdk.zip"
+        Write-Host "expanding JDK"
+        Expand-Archive -LiteralPath "$dtcw_path\jdk.zip" -DestinationPath "$dtcw_path\"
+        $firstArgsIndex = 1   # << Shift first param
+        exit 1
     }
     default {
         $firstArgsIndex = 0   # << Use all params        
@@ -117,7 +155,7 @@ elseif ($docker) {
     $docker_cmd = Get-Command docker
     Write-Host $docker_cmd
     $command = "$docker_cmd run --name doctoolchain${dockerVersion} -e DTC_HEADLESS=1 -e DTC_SITETHEME -p 8042:8042 --rm -it --entrypoint /bin/bash -v '${PWD}:/project' 'rdmueller/doctoolchain:v$dockerVersion' -c ""doctoolchain . $commandArgs $DTC_OPTS && exit"""
-
+    $doJavaCheck = $False
 }
 else {
     Write-Host "docToolchain $version is not installed."
@@ -154,5 +192,8 @@ you need docToolchain as CLI-Tool installed or docker.
     }
 }
 
-# Write-Host "Command to invoke: '$command'" # << line for debugging
+if ($doJavaCheck) {
+    checkJava
+}
+Write-Host "Command to invoke: '$command'" # << line for debugging
 Invoke-Expression "$command"
